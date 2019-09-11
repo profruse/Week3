2198511037099cadddda6eed829364765c1a9c66
profruse <morgansmom@gmail.com>
2019-09-10 19:42:41 -0500

    Last of class

--- main_source/measurement_converter.py
+++ main_source/measurement_converter.py
@@ -1,11 +1,20 @@
 """This programs take input of inches and converts to feet"""
 def convert_inches_to_feet(inches):
     INCHES_IN_FEET = 12
-    feet = inches / INCHES_IN_FEET
+    if inches > 0:
+        feet = inches / INCHES_IN_FEET
+    else:
+        feet = 0
     return int(feet)
     # fix test_twenty_four_inches
 
 
+def addition():
+    x = input("Enter a number")
+    print('x = ' + str(x)) # print debug statement
+    y = input("Enter another number")
+    return x + y
+
 if __name__ == '__main__':
     print("Hello!")
-    print("28 inches is " + str(convert_inches_to_feet(28)) + " feet")
+    print("-12 inches is " + str(convert_inches_to_feet(-12)) + " feet")
--- test/test_measurement_converter.py
+++ test/test_measurement_converter.py
@@ -1,5 +1,6 @@
 import unittest
 from main_source import measurement_converter
+import unittest.mock as mock
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,6 +16,15 @@ class MyTestCase(unittest.TestCase):
 
     def test_twenty_eight_inches(self):
         self.assertEqual(2, measurement_converter.convert_inches_to_feet(28))
+
+    def test_negative_twelve_inches(self):
+        self.assertEqual(0, measurement_converter.convert_inches_to_feet(-12))
+
+    def test_zero_inches(self):
+        self.assertEqual(0, measurement_converter.convert_inches_to_feet(0))
+    def test_one_plus_one(self):
+        with mock.patch('builtins.input',side_effect=[1,1]):
+            assert measurement_converter.addition() == 2
 
 
 if __name__ == '__main__':
